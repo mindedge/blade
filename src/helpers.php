40f20c56983abcc10190f2d3ae49841b6ff79e0a
Mike McGrath <mmcgrath@mindedge.com>
2019-06-25 00:37:30 -0400

    cleanup

--- src/helpers.php
+++ src/helpers.php
@@ -1051,45 +1051,6 @@ if (! function_exists('view')) {
         }
     }
     
-    if (! function_exists('env')) {
-        /**
-         * Gets the value of an environment variable.
-         *
-         * @param  string  $key
-         * @param  mixed   $default
-         * @return mixed
-         */
-        function env($key, $default = null)
-        {
-            $value = getenv($key);
-    
-            if ($value === false) {
-                return value($default);
-            }
-    
-            switch (strtolower($value)) {
-                case 'true':
-                case '(true)':
-                    return true;
-                case 'false':
-                case '(false)':
-                    return false;
-                case 'empty':
-                case '(empty)':
-                    return '';
-                case 'null':
-                case '(null)':
-                    return;
-            }
-    
-            if (($valueLength = strlen($value)) > 1 && $value[0] === '"' && $value[$valueLength - 1] === '"') {
-                return substr($value, 1, -1);
-            }
-    
-            return $value;
-        }
-    }
-    
     if (! function_exists('filled')) {
         /**
          * Determine if a value is "filled".
